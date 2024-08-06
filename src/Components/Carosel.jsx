fcfe9bbf513ac120f54de68e4f68dce980b629ad
Melosa <136803069+MelosaRao@users.noreply.github.com>
2024-08-06 09:54:35 -0400

    image attribution links added

--- src/Components/Carosel.jsx
+++ src/Components/Carosel.jsx
@@ -12,6 +12,18 @@ const ImageCarousel = () => {
     rings
   ];
 
+  const captions = [
+  ['Image by Kai Pilger on Pexcel', 'https://www.pexels.com/photo/assorted-clothes-996329/'],
+
+
+
+  ['Image by freepik', "https://www.freepik.com/free-photo/modern-stationary-collection-arrangement_23441437.htm#fromView=search&page=1&position=0&uuid=e1efa232-998a-4990-8d0c-a67599e855b5"],
+
+
+  ['Image by pikisuperstar on Freepik', 'https://www.freepik.com/free-vector/realistic-black-background-with-realistic-elements_20187420.htm#fromView=search&page=1&position=2&uuid=f7665004-0784-4e33-a507-ee20b1de0868']
+
+  ]
+
   const [currentImage, setCurrentImage] = React.useState(0);
 
   const nextImage = () => {
@@ -25,11 +37,13 @@ const ImageCarousel = () => {
 
   return (
     <div className="relative w-7/12 mx-auto">
+      <figure className="w-auto max-h-screen rounded-lg ">
       <img
         src={images[currentImage]}
         alt={`Image ${currentImage + 1}`}
-        className="w-auto max-h-screen rounded-lg "
       />
+        <caption className='w-max text-xs text-slate-500'><a href={captions[currentImage][1]}>{captions[currentImage][0]}</a></caption>
+      </figure>
       <div className="absolute top-1/2 left-0 transform -translate-y-1/2 flex justify-between w-full">
         <button
           onClick={prevImage}
